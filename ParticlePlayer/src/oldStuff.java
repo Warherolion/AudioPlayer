3d0e7644d14a48001f12bf678a0e1d11e9d99b4f
Warherolion <ranvir1412@gmail.com>
2020-05-28 02:00:25 -0400

    Inital commit base classes and methods created

---
+++ ParticlePlayer/src/oldStuff.java
@@ -0,0 +1,16 @@
+package src;
+
+public class oldStuff {
+    /*
+
+    fileList = new File[]{ProgramMap.musicFile, ProgramMap.musicFile1, ProgramMap.musicFile2, ProgramMap.musicFile3};
+
+    File[] fileList = new File[pathnames.length];
+    //Finds the music file
+    public static File musicFile = new File(  userDir +"/src/Music/Heartless.wav");
+    public static File musicFile1 = new File(userDir +"/src/Music/Life Is Good.wav");
+    public static File musicFile2 = new File(userDir +"/src/Music/Panini.wav");
+    public static File musicFile3 = new File(userDir +"/src/Music/ROXANNE.wav");
+     */
+
+}
